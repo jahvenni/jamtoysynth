45b35626298b348363f9130b85b87d8810e04bdf
Johannes Ahvenniemi <jahvenni@gmail.com>
2011-11-29 19:47:04 +0200

    Created pot groups This version causes some assertion fails when you try to change pot values. Don't know why.

--- src/pots.cpp
+++ src/pots.cpp
@@ -1,27 +1,49 @@
 #include "pots.h"
 
-static float radius = 30;
-static float padding = radius * 2;
-static std::list<pot_t> pots;
+static float radius = 0.03;
+static float padding = radius * 0.5;
+
+typedef struct potgroup_t {
+  std::list<pot_t> pots;
+  float x, y;
+  char* name;
+} potgroup_t;
+
+static potgroup_t groups[3];
+
 
 void pots_init(instrument_control_t* control)
 {
-  float x = screen_width / 2 - 3 * (radius * 2 + padding);
-  float y = screen_height / 4;
-  pot_t pot1 = { &(control->carrier_amplitude), x, y, "amp" };
-  pots.push_back(pot1);
-  x += radius * 2 + padding;
-  pot_t pot2 = { &(control->filter_freq), x, y, "freq" };
-  pots.push_back(pot2);
-  x += radius * 2 + padding;
-  pot_t pot3 = { &(control->filter_resonance), x, y, "res" };
-  pots.push_back(pot3);
-  x += radius * 2 + padding;
-  pot_t pot4 = { &(control->filter_gain), x, y, "gain" };
-  pots.push_back(pot4);
-  x += radius * 2 + padding;
-  pot_t pot5 = { &(control->modulator_amplitude), x, y, "amp" };
-  pots.push_back(pot5);
+  {
+    std::list<pot_t> pots;
+    pot_t pot1 = { &(control->carrier_amplitude), 0, 0, (char*)"amp" };
+    pots.push_back(pot1);
+    potgroup_t group = { pots, 0.1, 0.1, (char*)"general" };
+    groups[0] = group;
+  }
+
+  {
+    std::list<pot_t> pots;
+    pot_t pot1 = { &(control->filter_freq), -radius * 2 - padding, 0, (char*)"freq" };
+    pot_t pot2 = { &(control->filter_resonance), 0, 0, (char*)"res" };
+    pot_t pot3 = { &(control->filter_gain), radius * 2 + padding, 0, (char*)"gain" };
+    pots.push_back(pot1);
+    pots.push_back(pot2);
+    pots.push_back(pot3);
+    potgroup_t group = { pots, 0.5, 0.1, (char*)"filter" };
+    groups[1] = group;
+  }
+
+  {
+    std::list<pot_t> pots;
+    pot_t pot1 = { &(control->modulator_amplitude), -radius - padding / 2, 0, (char*)"amp" };
+    pot_t pot2 = { &(control->modulator_freq), radius + padding / 2, 0, (char*)"freq" };
+    pots.push_back(pot1);
+    pots.push_back(pot2);
+    potgroup_t group = { pots, 0.5, 0.4, (char*)"modulator" };
+    groups[2] = group;
+  }
+
 }
 
 void pots_set(pot_t* pot, float amt)
@@ -31,7 +53,6 @@ void pots_set(pot_t* pot, float amt)
 
 void pots_add(pot_t* pot, float amt)
 {
-  *(pot->value) += amt;
   if (*(pot->value) + amt <= 0.0) *(pot->value) = 0.0;
   else if (*(pot->value) + amt > 1.0) *(pot->value) = 1.0;
   else *(pot->value) += amt;
@@ -39,38 +60,42 @@ void pots_add(pot_t* pot, float amt)
 
 void pots_render()
 {
-
-  float x, y;
+  float r = radius * screen_width;
   glLoadIdentity();  
   glDisable( GL_TEXTURE_2D );
   glDisable( GL_BLEND);
-  for (std::list<pot_t>::iterator iter = pots.begin(); iter != pots.end(); iter++) {
-    assert(iter->value >= 0);
-    x = iter->x;
-    y = iter->y;
+  for (int i = 0; i < 3; i++) {
+    potgroup_t *group = &(groups[i]);
     glPushMatrix();
-    glTranslatef(x, y, 0);
-    glTranslatef(0, radius + 10, 0);
-    text_render(iter->name);
-    glTranslatef(0, -radius - 10, 0);
-    glRotatef(-150 + *(iter->value) * 300, 0.0, 0, 1);
-    glColor3f(0.5, 0.5, 0.2);
-    glDisable( GL_TEXTURE_2D );
-    glDisable( GL_BLEND);
-    glBegin(GL_TRIANGLE_FAN);
-    glVertex2f(0, 0);
-    for (float angle = 0; angle - 0.05 < 2 * M_PI; angle += 0.05) {
-      glVertex2f(sin(angle) * radius, cos(angle) * radius);
-    }
-    glEnd();
-    glColor3f(0.1, 0.1, 0);
-    glBegin(GL_QUADS);
-    glVertex2f(-radius / 10, 0);
-    glVertex2f( radius / 10, 0);
-    glVertex2f( radius / 10, -radius);
-    glVertex2f(-radius / 10, -radius);
-    glEnd();
+    glTranslatef((int)(group->x * screen_width), (int)(group->y * screen_height), 0);
+    text_render(group->name);
+    //    glTranslatef(0, 2 * r, 0);
+    for (std::list<pot_t>::iterator iter = group->pots.begin(); iter != group->pots.end(); iter++) {
+      glPushMatrix();
+      glTranslatef((int)(iter->x * screen_width), (int)(iter->y * screen_height), 0);
+      glTranslatef(0, r + 10, 0);
+      text_render(iter->name);
+      glTranslatef(0, -r - 10, 0);
+      glRotatef(-150 + *(iter->value) * 300, 0.0, 0, 1);
+      glColor3f(0.5, 0.5, 0.2);
+      glDisable( GL_TEXTURE_2D );
+      glDisable( GL_BLEND);
+      glBegin(GL_TRIANGLE_FAN);
+      glVertex2f(0, 0);
+      for (float angle = 0; angle - 0.05 < 2 * M_PI; angle += 0.05) {
+	glVertex2f(sin(angle) * r, cos(angle) * r);
+      }
+      glEnd();
+      glColor3f(0.1, 0.1, 0);
+      glBegin(GL_QUADS);
+      glVertex2f(-r / 10, 0);
+      glVertex2f( r / 10, 0);
+      glVertex2f( r / 10, -r);
+      glVertex2f(-r / 10, -r);
+      glEnd();
 
+      glPopMatrix();
+    }
     glPopMatrix();
   }
 }
@@ -78,13 +103,17 @@ void pots_render()
 
 pot_t* pots_get(float x, float y)
 {
-  x = x * screen_width;
-  y = y * screen_height;
-  for (std::list<pot_t>::iterator iter = pots.begin(); iter != pots.end(); ++iter) {
-    float dx = x - iter->x;
-    float dy = y - iter->y;
-    if (dx*dx + dy*dy < radius*radius)
-      return &(*iter);
+  for (int i = 0; i < 3; i++) {
+    potgroup_t *group = &(groups[i]);
+    std::list<pot_t> pots = group->pots;
+    for (std::list<pot_t>::iterator iter = pots.begin(); iter != pots.end(); ++iter) {
+      //The y distance has to be scaled to make the world a square
+      float dx = (x - (group->x + iter->x));
+      float dy = (y - (group->y + iter->y)) * (screen_height / (float)screen_width);
+      if (dx*dx + dy*dy < radius*radius) {
+	return &(*iter);
+      }
+    }
   }
   return NULL;
 }
